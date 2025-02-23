2d68e2bb98ed9bc6f276bf2450f674c632a825f0
이현빈 <lhb0107@naver.com>
2025-02-23 17:50:36 +0900

    Day6

---
+++ Day6/src/Mission/mission1.tsx
@@ -0,0 +1,25 @@
+export default function mission1() {
+  return (
+    <>
+      <pre>
+        <code>
+          {`
+
+
+[ 문제 소개 ]
+타입 단언을 이용해 person 변수에 빈 객체를 할당하세요
+(힌트. 초과 프로퍼티 검사 방지를 위한 타입 단언)
+
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+let person: Person = {} as Person;
+  `}
+        </code>
+      </pre>
+    </>
+  );
+}
---
+++ Day6/src/Mission/mission2.tsx
@@ -0,0 +1,26 @@
+export default function mission2() {
+  return (
+    <div>
+      <pre>
+        <code>{`/*
+[ 문제 소개 ]
+타입 단언을 이용해 아래 코드의 오류를 해결하세요
+(힌트. const 단언)
+*/
+
+let value = 10 as const;
+ OR
+let value : 10 = 10;
+giveMe10(value);
+
+/* [Test] 여기부터는 정답을 체크하기 위한 용도로 수정하지 않습니다 */
+function giveMe10(value: 10) {
+  return value;
+}
+
+
+`}</code>
+      </pre>
+    </div>
+  );
+}
