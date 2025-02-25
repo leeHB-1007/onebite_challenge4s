1c18b75c111146eef9ef930a889f4428d586a20d
이현빈 <lhb0107@naver.com>
2025-02-25 16:15:48 +0900

    day7

---
+++ Day7/src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <div>day7 미션 입니다.</div>
+      <Link to="/mission1">mission 1</Link>
+      <br />
+      <Link to="/mission2">mission 2</Link>
+      <br />
+
+
+      <br />
+    </>
+  );
+}
+
+export default App;
