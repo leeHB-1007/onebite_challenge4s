4ae3f073678655dbde95e2bdf0ab614dfae427fe
이현빈 <lhb0107@naver.com>
2025-03-03 22:10:31 +0900

    day14

---
+++ Day14/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <div>day14 미션 입니다.</div>
+      <Link to="/mission1">mission 1</Link>
+      <br />
+      <Link to="/mission2">mission 2</Link>
+      <br />
+      <Link to="/mission3">mission 3</Link>
+    </>
+  );
+}
+
+export default App;
---
+++ Day14/src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Mission1 from "./Mission/mission1.tsx";
+import Mission2 from "./Mission/mission2.tsx";
+import Mission3 from "./Mission/mission3.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/mission1" element={<Mission1 />} />
+      <Route path="/mission2" element={<Mission2 />} />
+      <Route path="/mission3" element={<Mission3 />} />
+    </Routes>
+  </BrowserRouter>
+);
